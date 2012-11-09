5e14f73ccfc70983c7dee9a571506e45ba5eb1a2
Yinan Na <nyn531@gmail.com>
2012-11-08 21:33:17 -0800

    change distance calculation

--- view/js/custom.js
+++ view/js/custom.js
@@ -37,13 +37,28 @@ function handle_geolocation_query_list(position){
     $.getJSON('../control/get_nearby_places.php?lat='+position.coords.latitude+'&lon='+position.coords.longitude, function(data) {
     	curLon = position.coords.longitude;
    		curLat = position.coords.latitude;
+
     	$.each(data, function(index, place) {
-    		distance = Math.sqrt(69.1*(curLat - place.lat)*69.1*(curLat - place.lat)+53.0*(curLon - place.lon)*53.0*(curLon - place.lon))/1600;
+    		//distance = Math.sqrt(69.1*(curLat - place.lat)*69.1*(curLat - place.lat)+53.0*(curLon - place.lon)*53.0*(curLon - place.lon))/1600;
+            dist = distance(curLat, curLon, place.lat, place.lon);
     		$('#place-list').append('<li> <a href="location_detail.php?place_id='+place.place_id+'">'+
 							'<img style="width:100%; height:100%; padding: 1px" src="'+place.pic_url+'"/>'+
 							'<h3>'+place.name+'</h3>'+
-							'<p>'+distance+' miles</p></a></li>');
+							'<p>'+dist+'</p></a></li>');
     	})
     	$('#place-list').listview('refresh');
     });
+}
+function distance(lat1,lon1,lat2,lon2) {
+    var R = 6371; // km (change this constant to get miles)
+    var dLat = (lat2-lat1) * Math.PI / 180;
+    var dLon = (lon2-lon1) * Math.PI / 180;
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    if (d>1) return Math.round(d)+"km";
+    else if (d<=1) return Math.round(d*1000)+"m";
+    return d;
 }
