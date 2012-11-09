b3855c8d08b02d55adc15e624ce7719872e86200
Yinan Na <nyn531@gmail.com>
2012-11-08 21:39:19 -0800

    map location

--- view/js/map_view.js
+++ view/js/map_view.js
@@ -1,101 +1,118 @@
-	// Set up Map
-      var markersArray = [];
-      var currLat = 37.484568564 ;
-      var currLon = -122.148;
+$("#page-map").live('pagebeforeshow', function(){
+	navigator.geolocation.getCurrentPosition(handle_geolocation_query_mapview, handle_errors);  
+})
 
-      var map; 
-      var showPicture = 1;
+function handle_geolocation_query_mapview(position){  
+    $.getJSON('../control/get_nearby_places.php?lat='+position.coords.latitude+'&lon='+position.coords.longitude, function(data) {
+    	curLon = position.coords.longitude;
+   		curLat = position.coords.latitude;
 
-			function showPosition(position) {
-  				currLat = position.coords.latitude;
-  				currLon = position.coords.longitude;
-			} 
+    	$.each(data, function(index, place) {
 
-	        function initialize() {
+    		//console.log(place.lat);
+    		//console.log(place.lon);
+    	})
+    });
+}
 
-			    //if (navigator.geolocation) {
-			    //   navigator.geolocation.getCurrentPosition(showPosition);
-			    //} 
-  
-		        var mapOptions = {
-		          center: new google.maps.LatLng(currLat,currLon),
-		          zoom: 10,
-		          mapTypeId: google.maps.MapTypeId.ROADMAP
-		        };
-		        map = new google.maps.Map(document.getElementById("map_canvas"),
-		            mapOptions);
-		        var image = 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc6/275013_100000981942170_4422755_q.jpg';
-		        image.height = 20;
-		        image.width = 20;
-		        var marker = new google.maps.Marker({
-		                position: new google.maps.LatLng(currLat,currLon),
-		                map: map,
-		                icon: image
-		            });  
-		        marker.setMap(map);
-		        
-		        markersArray.push(marker);
+// Set up Map
+var markersArray = [];
+var currLat = 37.484568564 ;
+var currLon = -122.148;
 
-		        var marker0 = new google.maps.Marker({
-		                position: new google.maps.LatLng(currLat-0.01,currLon+0.01),
-		                map: map
-		        });  
-		        marker0.setMap(map);
-		        
-		        markersArray.push(marker0);
+var map; 
+var showPicture = 1;
 
-		        var marker4 = new google.maps.Marker({
-		                position: new google.maps.LatLng(currLat-0.031,currLon+0.091),
-		                map: map
-		        });  
-		        marker4.setMap(map);
-		        
-		        markersArray.push(marker4);
+	function showPosition(position) {
+			currLat = position.coords.latitude;
+			currLon = position.coords.longitude;
+	} 
 
-		        var marker5 = new google.maps.Marker({
-		                position: new google.maps.LatLng(currLat-0.11,currLon+0.031),
-		                map: map,
-		                url: '#Tour'
-		        });  
+    function initialize() {
 
-		        google.maps.event.addListener(marker5, 'click', function() {
-			      window.location.href = marker5.url;
-			    });
+	    //if (navigator.geolocation) {
+	    //   navigator.geolocation.getCurrentPosition(showPosition);
+	    //} 
 
-		        marker5.setMap(map);
-		        
-		        markersArray.push(marker5);
+        var mapOptions = {
+          center: new google.maps.LatLng(currLat,currLon),
+          zoom: 10,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+        var image = 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc6/275013_100000981942170_4422755_q.jpg';
+        image.height = 20;
+        image.width = 20;
+        var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(currLat,currLon),
+                map: map,
+                icon: image
+            });  
+        marker.setMap(map);
+        
+        markersArray.push(marker);
+
+        var marker0 = new google.maps.Marker({
+                position: new google.maps.LatLng(currLat-0.01,currLon+0.01),
+                map: map
+        });  
+        marker0.setMap(map);
+        
+        markersArray.push(marker0);
+
+        var marker4 = new google.maps.Marker({
+                position: new google.maps.LatLng(currLat-0.031,currLon+0.091),
+                map: map
+        });  
+        marker4.setMap(map);
+        
+        markersArray.push(marker4);
+
+        var marker5 = new google.maps.Marker({
+                position: new google.maps.LatLng(currLat-0.11,currLon+0.031),
+                map: map,
+                url: '#Tour'
+        });  
+
+        google.maps.event.addListener(marker5, 'click', function() {
+	      window.location.href = marker5.url;
+	    });
+
+        marker5.setMap(map);
+        
+        markersArray.push(marker5);
 
 
-	      } 
- 
-        var timer = 100;
+  } 
 
-		window.onload = function changeQuantity(){
-			initialize();
+var timer = 100;
 
-			if (window.DeviceMotionEvent==undefined) {
-				showPicture = 0;
-				document.getElementById('bessie').style.display = 'block';
+window.onload = function changeQuantity(){
+	initialize();
 
-		    } else {
-		    	window.ondevicemotion = function(event) {
-		    	document.getElementById('bill').style.display = 'block';
-				
-		    		ax = event.accelerationIncludingGravity.x;
-		    		ay = event.accelerationIncludingGravity.y;
+	if (window.DeviceMotionEvent==undefined) {
+		showPicture = 0;
+		document.getElementById('bessie').style.display = 'block';
 
-			        var gravity = Math.abs(event.accelerationIncludingGravity.x)+Math.abs(event.accelerationIncludingGravity.y)+Math.abs(event.accelerationIncludingGravity.z);
-			        timer = timer+1;
-			        if(gravity>30 && timer >100)
-			        {
-			          timer = 0;
-			          for (var i = 0; i < markersArray.length; i++ ) {
-			              markersArray[i].setMap(null);
-			          }
+    } else {
+    	window.ondevicemotion = function(event) {
+    	document.getElementById('bill').style.display = 'block';
+		
+    		ax = event.accelerationIncludingGravity.x;
+    		ay = event.accelerationIncludingGravity.y;
 
-			        }
-		    	}
-		    }
+	        var gravity = Math.abs(event.accelerationIncludingGravity.x)+Math.abs(event.accelerationIncludingGravity.y)+Math.abs(event.accelerationIncludingGravity.z);
+	        timer = timer+1;
+	        if(gravity>30 && timer >100)
+	        {
+	          timer = 0;
+	          for (var i = 0; i < markersArray.length; i++ ) {
+	              markersArray[i].setMap(null);
+	          }
 
-		}
+	        }
+    	}
+    }
+
+}
